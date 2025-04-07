4d8df18c26774e44c964b959f30275372419713c
Bruno Azevedo <brunoazvd@protonmail.com>
2025-04-07 13:11:13 -0300

    updated links, added favicon, change about-me picture, changed primary color

--- src/components/Navbar/NavLink.jsx
+++ src/components/Navbar/NavLink.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export const NavLink = ({children, ...props}) => {
   return (
-    <Link {...props} className="text-2xl hover:text-primary">
+    <Link {...props} className="text-2xl hover:text-primary-light">
         {children}
     </Link>
   )
